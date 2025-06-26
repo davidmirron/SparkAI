6816204ff4ddbadc4d9d542ce8b6db9ba966ffed
david <davidsmiron@gmail.com>
2025-06-26 15:54:25 +0300

    Initial Commit

---
+++ SparkAI/SparkAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparkAIApp.swift
+//  SparkAI
+//
+//  Created by David Miron on 26.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct SparkAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SparkAITests/SparkAITests.swift
@@ -0,0 +1,17 @@
+//
+//  SparkAITests.swift
+//  SparkAITests
+//
+//  Created by David Miron on 26.06.2025.
+//
+
+import Testing
+@testable import SparkAI
+
+struct SparkAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
